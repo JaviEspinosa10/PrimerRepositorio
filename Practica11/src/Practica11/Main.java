9f128e9b8e92f367de4c499908292f6ec4466bb2
Javi <daw202509@estudiantes.salesianasnsp.es>
2024-03-20 10:14:02 +0100

    descripcion de la instantanea

---
+++ Practica11/src/Practica11/Main.java
@@ -0,0 +1,13 @@
+package Practica11;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String nombre="Javier";
+		
+		System.out.println("Esto es un ejemplo de GIT");
+		System.out.println("Mi nombre: " + nombre);
+
+	}
+
+}
